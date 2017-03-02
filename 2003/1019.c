0cc0bd26343d2658048ccd3e782b06abfcf9890b
YiSheng <upsetboyincludeme830820@gmail.com>
2017-03-02 12:17:21 +0800

    1019-ac

--- 2003/1019.c
+++ 2003/1019.c
@@ -13,10 +13,10 @@
 #include <string.h>
 #include <stdlib.h>
 
-char origin_char[100], final_char[100];
-int origin_bit[100], final_bit[100];
-int switch_state[100];
-int swithes_decimal[100];
+char origin_char[400], final_char[400];
+int origin_bit[400], final_bit[400];
+int switch_state[400];
+int swithes_decimal[400];
 
 void StringToBitArray(int p);
 void BitArrayToString();
@@ -25,9 +25,10 @@ int simulator();
 
 void init() {
 	int i;
-	for(i = 0; i < 100; i++) {
+	for(i = 0; i < 400; i++) {
 		origin_bit[i] = final_bit[i] = 0;
 		switch_state[i] = 0;
+		swithes_decimal[i] = 0;
 	}
 }
 
@@ -36,23 +37,23 @@ int main() {
 	while(1) {
 		init();
 		
-
 		scanf("%s%s", origin_char, final_char);
 		if(origin_char[0] == '0' && final_char[0] == '0')
 			break;
-		if(caseNum) puts("");
+
 		StringToBitArray(0);
 		StringToBitArray(1);
 
 		int success = simulator();
 
 		BitArrayToString();
-		
+
+		if(caseNum) puts("");
 		printf("Case Number %d: ",++caseNum);
 		if(success) {
 			int i;
 			int start = 0;
-			for(i = 0; i < 100; i++) {
+			for(i = 0; i < 400; i++) {
 				if(swithes_decimal[i] != 0) start = 1;
 				if(start) printf("%d", swithes_decimal[i]);
 			}
@@ -69,52 +70,51 @@ int simulator() {
 	int i, j;
 	int success = 1;
 
-	int maxlen = 0;
-
-	for(i = 0; i < 100; i++) {
-		if(origin_bit[i] != 0 || final_bit[i] != 0) {
-			maxlen = i;
+	int startindex = 0;
+	int light[400];
+	for(i = 0; i < 400; i++) {
+		if(origin_bit[i] == 1 || final_bit[i] == 1) {
+			startindex = i;
 			break;
 		}
 	}
 
-	if(maxlen == 99) {
-		if(origin_bit[99] == final_bit[99])
+
+	if(startindex == 399) {
+		if(origin_bit[399] == final_bit[399])
 			return 1;
 		else{
-			switch_state[99] = 1;
+			switch_state[399] = 1;
 			return 1;
 		}
 	}
 
 	for(j = 0; j < 2; j++) {
+		/* prevent polluting origin state */
+		for(i = 0; i < 400; i++) {
+			light[i] = origin_bit[i];
+			switch_state[i] = 0;
+		}
+
 		if(j == 1) {
-			switch_state[maxlen] = 1;
-			origin_bit[maxlen] = abs(origin_bit[maxlen]-1);
-			origin_bit[maxlen+1] = abs(origin_bit[maxlen+1]-1);
+			switch_state[startindex] = 1;
+			light[startindex] = (int)abs(light[startindex]-1);
+			light[startindex+1] = (int)abs(light[startindex+1]-1);
 		}
 		
-		for(i = maxlen+1; i < 100; i++) {
-			if(i == 99) {
-				if(origin_bit[98] != final_bit[98]) {
-					switch_state[99] = 1;
-					origin_bit[98] = abs(origin_bit[98]-1);
-					origin_bit[99] = abs(origin_bit[99]-1);
-				}
-			}
-			else {
-				if(origin_bit[i-1] != final_bit[i-1]) {
-					switch_state[i] = 1;
-					origin_bit[i-1] = abs(origin_bit[i-1]-1);
-					origin_bit[i] = abs(origin_bit[i]-1);
-					origin_bit[i+1] = abs(origin_bit[i+1]-1);
-				}
+		for(i = startindex+1; i < 400; i++) {
+			if(light[i-1] != final_bit[i-1]) {
+				switch_state[i] = 1;
+				light[i-1] = (int)abs(light[i-1]-1);
+				light[i] = (int)abs(light[i]-1);
+				if(i+1 < 400)
+					light[i+1] = (int)abs(light[i+1]-1);
 			}
 		}
 
 		success = 1;
-		for(i = 0; i < 100; i++) {
-			if(origin_bit[i] != final_bit[i])
+		for(i = 0; i < 400; i++) {
+			if(light[i] != final_bit[i])
 				success = 0;
 		}
 		if(success) break;
@@ -125,18 +125,19 @@ int simulator() {
 }
 
 void StringToBitArray(int p) {
-	int origin_num[100];
-	int reversed_bits[100];
+	int origin_num[400];
+	int reversed_bits[400];
 	int i, j, k, l;
 
 	/* initialize */
-	for(i = 0; i < 100; i++)
+	for(i = 0; i < 400; i++)
 		origin_num[i] = reversed_bits[i] = 0;
 
 	int len;
 	if(p == 0) len = (int)strlen(origin_char);
 	else len = (int)strlen(final_char);
-	for(i = 100-len, j = 0; i < 100; i++, j++) {
+
+	for(i = 400-len, j = 0; i < 400; i++, j++) {
 		if(p == 0) origin_num[i] = origin_char[j] - '0';
 		else origin_num[i] = final_char[j] - '0';
 	}
@@ -144,12 +145,12 @@ void StringToBitArray(int p) {
 	/* start StringToBitArray */
 
 	int finish = 4*len;
-	if(finish>100) finish = 100;
+	if(finish>400) finish = 400;
 	int count = 0;
 	while(finish --) {
 		int quotient = 0, rest = 0;
 
-		for(i = 0; i < 100; i++) {
+		for(i = 0; i < 400; i++) {
 			
 			origin_num[i] += rest * 10;
 
@@ -161,7 +162,7 @@ void StringToBitArray(int p) {
 		reversed_bits[count++] = rest;
 	}
 
-	for(i = 99, j = 0; j < 100; i--, j++) {
+	for(i = 399, j = 0; j < 400; i--, j++) {
 		if(p == 0) origin_bit[i] = reversed_bits[j];
 		else final_bit[i] = reversed_bits[j];
 	}
@@ -171,16 +172,16 @@ void BitArrayToString() {
 
 	int i, j, k;
 	/* initialize */
-	for(i = 0; i < 100; i++)
+	for(i = 0; i < 400; i++)
 		swithes_decimal[i] = 0;
 	
-	for(i = 0; i < 100; i++) {
-		swithes_decimal[99] += switch_state[i];
-		if(i == 99) break;
-		for(j = 99; j >= 0; j--) {
+	for(i = 0; i < 400; i++) {
+		swithes_decimal[399] += switch_state[i];
+		if(i == 399) break;
+		for(j = 399; j >= 0; j--) {
 			swithes_decimal[j] *= 2;
 		}
-		for(j = 99; j >= 0; j--) {
+		for(j = 399; j >= 0; j--) {
 			if(swithes_decimal[j] > 9) {
 				swithes_decimal[j-1] += swithes_decimal[j] /10;
 				swithes_decimal[j] = swithes_decimal[j] % 10;
--- 2003/README.md
+++ 2003/README.md
@@ -7,3 +7,8 @@ The file is a part of problem 1019.
 problem 1019, i get "wa" in this version.  
 choose 0 in first loop, and choose 1 in second loop.  
 however, there are still some bugs that need to be figured out.  
+
+##20170302
+problem 1019 ac  
+first, i cannot pollute the origin input  
+second, i need to initialize the switches' state each time  
